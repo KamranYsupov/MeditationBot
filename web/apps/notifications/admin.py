35371df02493b6645248b1b3f5c59cddddc6b12b
KamranYsupov <kamrankek48@gmail.com>
2025-04-21 15:37:19 +0400

    убрал обязательный выбор файла при рассылке

--- web/apps/notifications/admin.py
+++ web/apps/notifications/admin.py
@@ -5,5 +5,7 @@ from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
+    filter_horizontal = ('receivers',)
+
     def has_change_permission(self, request, obj=None):
         return False
--- web/apps/notifications/signals.py
+++ web/apps/notifications/signals.py
@@ -16,24 +16,32 @@ def send_notification_after_creation(sender, instance: Notification, created, **
     if not created:
         return
 
-    file_ext = os.path.splitext(instance.file.name)[1].lower()
-    if file_ext == '.mp4':
-        method, file_type = ('sendVideo', 'video')
-    elif file_ext == '.mp3':
-        method, file_type = ('sendAudio', 'audio')
-    elif file_ext in ('.jpg', '.jpeg', '.png'):
-        method, file_type = ('sendPhoto', 'photo')
-    else:
-        return
-
     def send_notification():
-        for telegram_user in instance.receivers.all():
-            telegram_service.send_file(
-                chat_id=telegram_user.telegram_id,
+        if not instance.file:
+            telegram_method = telegram_service.send_message
+            telegram_method_kwargs = dict(text=instance.text)
+        else:
+            file_ext = os.path.splitext(instance.file.name)[1].lower()
+            if file_ext == '.mp4':
+                method, file_type = ('sendVideo', 'video')
+            elif file_ext == '.mp3':
+                method, file_type = ('sendAudio', 'audio')
+            elif file_ext in ('.jpg', '.jpeg', '.png'):
+                method, file_type = ('sendPhoto', 'photo')
+            else:
+                return
+
+            telegram_method = telegram_service.send_file
+            telegram_method_kwargs = dict(
                 file_path=instance.file.path,
                 file_type=file_type,
                 method=method,
-                caption=instance.text
+                caption=instance.text,
             )
+
+        for telegram_user in instance.receivers.all():
+            telegram_method_kwargs['chat_id'] = telegram_user.telegram_id
+            print(telegram_method_kwargs)
+            telegram_method(**telegram_method_kwargs)
 
     transaction.on_commit(send_notification)
